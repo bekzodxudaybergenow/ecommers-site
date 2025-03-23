ceb0ce23007e7c6117f7ccc06326917c6a596dc8
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2025-03-23 13:12:35 +0500

    Add a flexible part of the detailed section

--- src/components/Pages/ProductDetail.jsx
+++ src/components/Pages/ProductDetail.jsx
@@ -18,16 +18,16 @@ function ProductDetail() {
 
   return (
     <div className="ProductDetail my-20">
-      <div className="container flex gap-x-15 justify-center">
-        <img className="shadow-md w-[400px]" src={productList.thumbnail} alt={productList.title} />
+      <div className="container flex gap-x-15 justify-center max-[720px]:flex-col max-[720px]:justify-center">
+        <img className="shadow-md w-[400px] max-[720px]:w-[350px] max-[720px]:mx-auto max-[720px]:mb-10" src={productList.thumbnail} alt={productList.title} />
         <div className="product-detail--content">
-          <h2 className="text-[50px] font-bold">{productList.title}</h2>
-          <div className="flex gap-x-4 mb-10">
+          <h2 className="text-[50px] font-bold max-[840px]:text-[28px] max-[720px]:text-center max-[375px]:mb-6">{productList.title}</h2>
+          <div className="flex gap-x-4 mb-10 max-[840px]:mb-5 max-[720px]:justify-center">
             <div>Brand: <span className="font-bold">{productList.brand}</span></div>
             <div>Category: <span className="font-bold">{productList.category}</span></div>
             <div>Price: <span className="font-bold">${productList.price}</span></div>
           </div>
-          <p className="max-w-[500px]">{productList.description}</p>
+          <p className="max-w-[500px] max-[720px]:text-center max-[720px]:mx-auto">{productList.description}</p>
         </div>
       </div>
     </div>
